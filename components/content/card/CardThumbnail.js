4d6bc0d1552028e59ce0994f561ba9693abfd0b7
Robin Granqvist <robin.granqvist@hotmail.se>
2022-12-06 23:27:52 +0000

    fixed tooltips

--- components/content/card/CardThumbnail.js
+++ components/content/card/CardThumbnail.js
@@ -4,7 +4,7 @@ export default function CardThumbnail({ item }) {
   return (
     <div className="card__thumbnail-wrapper">
       <div className="card__thumbnail--shadow"></div>
-      <CardToolTip url={"/insp" + item.frontmatter.url__pretty} />
+      <CardToolTip url={item.frontmatter.url__pretty} />
 
       <img
         src={item.frontmatter.thumbnail}
