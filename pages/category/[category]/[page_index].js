825eaf6fa023399749abd6086fe006543dfc43e0
Robin Granqvist <robin.granqvist@hotmail.se>
2022-12-08 17:37:16 +0000

    increased category page grid item limit

--- pages/category/[category]/[page_index].js
+++ pages/category/[category]/[page_index].js
@@ -3,7 +3,7 @@ import path from "path";
 import matter from "gray-matter";
 import { numPages, getCategoryInspoItems } from "@/utils/index";
 
-const POSTS_PER_PAGE = 2;
+const POSTS_PER_PAGE = 24;
 
 import {
   HeadTag,
